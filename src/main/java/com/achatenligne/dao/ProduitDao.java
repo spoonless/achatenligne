9553b8fca3117cdc2af62c69a9c9f633f4331e84
David <dagaydevel@free.fr>
2019-05-14 14:17:21 +0200

    chargement d'un produit à partir de son id

--- src/main/java/com/achatenligne/dao/ProduitDao.java
+++ src/main/java/com/achatenligne/dao/ProduitDao.java
@@ -6,24 +6,45 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.achatenligne.model.Produit;
 
 public class ProduitDao {
 
+	private static final String SELECT_ALL_PRODUITS = "select id, code, libelle, prix from produit";
+
 	public List<Produit> getAll() {
-		try (Connection connection = ProduitDataSource.getSingleton().getConnection(); 
-		     Statement stmt = connection.createStatement()) {
+		try (Connection connection = ProduitDataSource.getSingleton().getConnection();
+				Statement stmt = connection.createStatement()) {
 
 			List<Produit> produits = new ArrayList<Produit>();
-			try (ResultSet rs = stmt.executeQuery("select id, code, libelle, prix from produit")) {
+			try (ResultSet rs = stmt.executeQuery(SELECT_ALL_PRODUITS)) {
 				while (rs.next()) {
-					Produit p = new Produit(rs.getInt("id"), rs.getString("code"), 
-							                rs.getString("libelle"), rs.getBigDecimal("prix"));
+					Produit p = new Produit(rs.getInt("id"), rs.getString("code"), rs.getString("libelle"),
+							rs.getBigDecimal("prix"));
 					produits.add(p);
 				}
 			}
 			return produits;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public Optional<Produit> getById(int id) {
+		try (Connection connection = ProduitDataSource.getSingleton().getConnection();
+			 Statement stmt = connection.createStatement()) {
+
+			try (ResultSet rs = stmt.executeQuery(SELECT_ALL_PRODUITS + " where id = " + id)) {
+				if (rs.next()) {
+					Produit p = new Produit(rs.getInt("id"), rs.getString("code"), rs.getString("libelle"),
+							rs.getBigDecimal("prix"));
+					return Optional.of(p);
+				}
+			}
+			return Optional.empty();
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
--- src/main/java/com/achatenligne/model/ProduitService.java
+++ src/main/java/com/achatenligne/model/ProduitService.java
@@ -1,14 +1,15 @@
 package com.achatenligne.model;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.achatenligne.dao.ProduitDao;
 
 public class ProduitService {
 	
 	public List<Produit> getAll() {
-		ProduitDao produiDao = new ProduitDao();
-		return produiDao.getAll();
+		ProduitDao produitDao = new ProduitDao();
+		return produitDao.getAll();
 	}
 
 	public Commande creerCommande(int...idProduits) {
@@ -20,10 +21,11 @@ public class ProduitService {
 	}
 
 	private void ajouter(Commande commande, int idProduit) {
-		for (Produit produit : getAll()) {
-			if (produit.getId() == idProduit) {
-				commande.add(produit);
-			}
+		ProduitDao produitDao = new ProduitDao();
+		Optional<Produit> produit = produitDao.getById(idProduit);
+		
+		if (produit.isPresent()) {
+			commande.add(produit.get());
 		}
 	}
 }
